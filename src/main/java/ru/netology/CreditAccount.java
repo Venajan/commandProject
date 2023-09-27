061e25fa7f7d4866865040128c6a70c08b7eec43
Venajan <pokrovskiyalex77@gmail.com>
2023-09-27 17:57:36 +0300

    Ready

---
+++ src/main/java/ru/netology/CreditAccount.java
@@ -0,0 +1,106 @@
+package ru.netology;
+
+/**
+ * Кредитный счёт
+ * Может иметь баланс вплоть до отрицательного, но до указанного кредитного лимита.
+ * Имеет ставку - количество процентов годовых на сумму на балансе, если она меньше нуля.
+ */
+public class CreditAccount extends Account {
+    protected int creditLimit;
+
+    /**
+     * Создаёт новый объект кредитного счёта с заданными параметрами.
+     * Если параметры некорректны (кредитный лимит отрицательный и так далее), то
+     * должно выкидываться исключения вида IllegalArgumentException.
+     *
+     * @param initialBalance - неотрицательное число, начальный баланс для счёта
+     * @param creditLimit    - неотрицательное число, максимальная сумма которую можно задолжать банку
+     * @param rate           - неотрицательное число, ставка кредитования для расчёта долга за отрицательный баланс
+     */
+    public CreditAccount(int initialBalance, int creditLimit, int rate) {
+        if (rate < 0) {
+            throw new IllegalArgumentException(
+                    "Накопительная ставка не может быть отрицательной, а у вас: " + rate
+            );
+        }
+        if (creditLimit < 0) {
+            throw new IllegalArgumentException(
+                    "Начальный кредитный лимит не может быть отрицательный, а у вас: " + creditLimit
+            );
+        }
+        if (initialBalance < 0) {
+            throw new IllegalArgumentException(
+                    "Начальный баланс не может быть отрицательный, а у вас: " + initialBalance
+            );
+        }
+        this.balance = initialBalance;
+        this.creditLimit = creditLimit;
+        this.rate = rate;
+    }
+
+    /**
+     * Операция оплаты с карты на указанную сумму.
+     * В результате успешного вызова этого метода, баланс должен уменьшиться
+     * на сумму покупки. Если же операция может привести к некорректному
+     * состоянию счёта (например, баланс может уйти меньше чем лимит), то операция должна
+     * завершиться вернув false и ничего не поменяв на счёте.
+     *
+     * @param amount - сумма покупки
+     * @return true если операция прошла успешно, false иначе.
+     */
+    @Override
+    public boolean pay(int amount) {
+        if (amount <= 0) {
+            return false;
+        }
+        if (balance + creditLimit >= amount) {
+            balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Операция пополнения карты на указанную сумму.
+     * В результате успешного вызова этого метода, баланс должен увеличиться
+     * на сумму покупки. Если же операция может привести к некорректному
+     * состоянию счёта, то операция должна
+     * завершиться вернув false и ничего не поменяв на счёте.
+     *
+     * @param amount - сумма пополнения
+     * @param amount
+     * @return true если операция прошла успешно, false иначе.
+     * @return
+     */
+    @Override
+    public boolean add(int amount) {
+        if (amount <= 0) {
+            return false;
+        }
+        balance += amount;
+        return true;
+    }
+
+    /**
+     * Операция расчёта процентов на отрицательный баланс счёта при условии, что
+     * счёт не будет меняться год. Сумма процентов приводится к целому
+     * числу через отбрасывание дробной части (так и работает целочисленное деление).
+     * Пример: если на счёте -200 рублей, то при ставке 15% ответ должен быть -30.
+     * Пример 2: если на счёте 200 рублей, то при любой ставке ответ должен быть 0.
+     *
+     * @return
+     */
+    @Override
+    public int yearChange() {
+        if (balance < 0) {
+            return balance / 100 * rate;
+        } else {
+            return 0;
+        }
+    }
+
+    public int getCreditLimit() {
+        return creditLimit;
+    }
+}
