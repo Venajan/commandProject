1b4fe27340ab0337a09cda9e1c1ba5b4ca463cf7
Venajan <pokrovskiyalex77@gmail.com>
2023-09-28 06:43:39 +0300

    Excluded setter

--- src/main/java/ru/netology/Account.java
+++ src/main/java/ru/netology/Account.java
@@ -24,7 +24,7 @@ public class Account {
         return rate;
     }
 
-    public void setRate(int rate) {
-        this.rate = rate;
-    }
+    //public void setRate(int rate) {
+    //    this.rate = rate;
+    // }
 }
